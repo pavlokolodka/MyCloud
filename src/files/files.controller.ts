6778369f72dcb9b2a099f5ee61920fd2e725947f
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-07-27 12:06:16 +0300

    Add create directory controller

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,64 +1,55 @@
-import { Router } from "express";
-import StorageService from './files.service';
+import express, { Router } from "express";
+import FileService from './files.service';
 
 
 
-class StorageController {
+class FileController {
   private path = '/storage';
-  private router = Router();
-  private storageService: StorageService;
-  // private posts: Post[] = [
-  //   {
-  //     author: 'Marcin',
-  //     content: 'Dolor sit amet',
-  //     title: 'Lorem Ipsum',
-  //   }
-  // ];
+  public router = Router();
+  private fileService: FileService;
  
   constructor() {
-    this.storageService = new StorageService();
+    this.fileService = new FileService();
     this.intializeRoutes();
   }
  
   public intializeRoutes() {
-    this.router.get(this.path, async (req, res) => {
-      // const files = await this.storageService.getAll();
-    });
-
     this.router.get(`${this.path}/:id`, async (req, res) => {
       const id = req.params.id;
       
-      const link = await this.storageService.getLink(`${id}`);
+      const link = await this.fileService.getLink(`${id}`);
      
       return res.send(link);
     });
 
+    // this.router.get(this.path, (req, res) => {
+    //   return res.send('hi!')
+    // });
+    
+    this.router.post(this.path, async (req, res) => {
+      const file = await this.fileService.createDirectory(req, res);
+
+      return res.send(file);
+    })
+
     this.router.post(`${this.path}/create`, async (req, res) => {
-      const file = await this.storageService.create(req);
+      const file = await this.fileService.create(req, res);
+
       return res.send(file);
     });
 
-    this.router.put(`${this.path}/:id/update`, async (req, res) => {
-      // const file = await this.storageService.update(req);
-      
-    });
+    
 
-    this.router.delete(`${this.path}/:id/delete`, async (req, res) => {
-      // const file = await this.storageService.delete(req);
+    // this.router.put(`${this.path}/:id/update`, async (req, res) => {
+    //   // const file = await this.fileService.update(req);
       
-    });
+    // });
+
+    // this.router.delete(`${this.path}/:id/delete`, async (req, res) => {
+    //   // const file = await this.fileService.delete(req);
+      
+    // });
   }
- 
-  // getAllPosts = (request: express.Request, response: express.Response) => {
-  //   response.send(this.posts);
-  // }
- 
-  // createAPost = (request: express.Request, response: express.Response) => {
-  //   const post: Post = request.body;
-  //   this.posts.push(post);
-  //   response.send(post);
-  // }
 }
 
-
-export default StorageController;
+export default FileController;
