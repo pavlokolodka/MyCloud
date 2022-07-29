d2883ea0eed7074ec36f3d50136fb5e3b2014278
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-07-29 21:50:44 +0300

    Add object id validation

---
+++ src/utils/isObjectId.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+export function isValidObjectId(id: string) {
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    if ( ( String(new mongoose.Types.ObjectId(id)) ) === id)
+        return true;
+    return false;
+  }
+  
+  return false;
+}
