f72c98ad29d8b01336564cf373c40601e915fab7
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-08-04 12:32:01 +0300

    Update file interface

--- src/files/model/files.interface.ts
+++ src/files/model/files.interface.ts
@@ -1,11 +1,17 @@
-import { ObjectId } from "mongoose";
+import mongoose from "mongoose";
 
 
 
 export interface IFile {
-  _id?: ObjectId,
+  _id?: mongoose.Types.ObjectId,
   name: string,
-  storageId: string,
-  link: string,
-  date?: Date
+  storageId?: string, // | null
+  link?: string,
+  size?: number,
+  date?: Date,
+  updatedAt?: Date,
+  type: string,
+  childs?: IFile['_id'][], // / []
+  parent: IFile['_id'] | null,
+  save?: () => Promise<this>;
 }
