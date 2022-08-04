200832f69f11bf0c40cc5caf188c51fb0b54a81d
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-08-04 12:27:47 +0300

    Add file repository

---
+++ src/files/model/files.repository.ts
@@ -0,0 +1,81 @@
+import { HttpError } from '../../utils/Error';
+import { IFileRepository } from './files.interface-repository';
+import { IFile } from './files.interface';
+import { File } from './files.model';
+
+export class FileRepository implements IFileRepository<IFile> {
+
+  constructor(private database = File) {}
+
+
+  public async create(query: IFile) {
+    try {
+      return await this.database.create(query);
+    } catch (e) {
+      throw new HttpError('creation error', 500)
+    }
+  }
+
+
+  public async getAll(query: object = {}, sortBy: string) {
+    try {
+      let files;
+      switch (sortBy) {
+        case 'name':
+          files = await this.database.find(query).sort({name: 1});
+          break;
+        case 'type':
+          console.log('here')
+          files = await this.database.find(query).sort({type: 1});
+          break;
+        case 'date':
+          files = await this.database.find(query).sort({date: 1});
+          break;
+        default:
+          files = await this.database.find(query);//.populate('childs').populate('parent');
+          break;
+    }
+    return files;
+    } catch (e) {
+      throw new HttpError('can not get files', 500)
+    }
+  }
+
+
+  public async getOne(query: object) {
+    try {
+      const file = await this.database.findOne(query)//.populate('parent')//.populate('childs');
+      return file;
+    } catch (e) {
+      throw new HttpError('can not get file', 500)
+    }
+  }
+
+
+  public async update(query: IFile) {
+    try {
+      return await this.database.updateOne(query);
+    } catch (e) {
+      throw new HttpError('update error', 500)
+    }
+  }
+
+
+  public async delete(query: object) {
+    try {
+      return await this.database.deleteOne(query);
+    } catch (e) {
+      throw new HttpError('deletion error', 500)
+    }
+  }
+
+  public async deleteParent(query: object, action: object) {
+    try {
+      return await this.database.updateOne(query, action);
+    } catch (e) {
+      throw new HttpError('update error', 500)
+    }
+  }
+
+ 
+}
