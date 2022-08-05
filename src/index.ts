41da76e37464b78d0f1e1e5a4912c261463d7af4
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-08-05 13:49:58 +0300

    Connect auth controller

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import 'dotenv/config'
 import FileController from './files/files.controller';
 import Server from './configuration/server';
+import { AuthController } from './auth/auth.controller';
 
 const port = Number(process.env.PORT) || 3000;
 
 
 const app = new Server(
   [
-    new FileController()
+    new FileController(),
+    new AuthController()
   ],
   port
 );
