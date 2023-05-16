f86417c1a918c890a55feb4427ad86ac0204f326
Pavlo Kolodka <pavlo.kolodka@chisw.com>
2023-05-16 09:45:40 +0300

    Test change for CI

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ const port = Number(process.env.PORT) || 5000;
 const authController = ControllerFactory.createController(AuthModule);
 const fileController = ControllerFactory.createController(FileModule);
 const app = new Server([...fileController, ...authController], port);
-
+// new comment
 app.listen();
