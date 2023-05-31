cdfe632023ed4011fadeb3cd6126be8da24ef25c
Pavlo Kolodka <pavlo.kolodka@chisw.com>
2023-05-31 16:18:59 +0300

    Remove polling

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,2 +1,2 @@
-export const secretKey = process.env.SECRET_KEY!; // new change in src .
+export const secretKey = process.env.SECRET_KEY!;
 export const refreshSecretKey = process.env.REFRESH_SECRET_KEY!;
