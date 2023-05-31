7639855cf628081025661cf85f7ce365b3437ec3
Pavlo Kolodka <pavlo.kolodka@chisw.com>
2023-05-31 15:19:12 +0300

    Test deploy process

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,2 +1,2 @@
-export const secretKey = process.env.SECRET_KEY!; // test comment
+export const secretKey = process.env.SECRET_KEY!; // new change in src .
 export const refreshSecretKey = process.env.REFRESH_SECRET_KEY!;
